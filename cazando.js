dab2b3139c686e05d1f2534b2c8c25612e6971fc
Ariell-jsj <king4r13l@gmail.com>
2026-04-24 20:04:12 -0500

    Crear funcion iniciarJuego-crear varibles y const

--- cazando.js
+++ cazando.js
@@ -1,9 +1,41 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
+// VARIABLES
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+// CONSTANTES
+const ANCHO_GATO = 60;
+const ALTO_GATO = 40;
+
+const ANCHO_COMIDA = 40;
+const ALTO_COMIDA = 40;
+
+// INICIAR JUEGO
+function iniciarJuego() {
+  // Gato centrado
+  gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+  gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+
+  // Comida esquina inferior derecha
+  comidaX = canvas.width - ANCHO_COMIDA;
+  comidaY = canvas.height - ALTO_COMIDA;
+
+  graficarGato();
+  graficarComida();
+}
+
 // Dibujar gato
 function graficarGato() {
   ctx.fillStyle = "orange";
-  // Rectángulo centrado
-  ctx.fillRect(225, 225, 50, 50);
+  ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+}
+
+// Dibujar comida
+function graficarComida() {
+  ctx.fillStyle = "green";
+  ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
 }
