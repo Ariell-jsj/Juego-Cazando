ddec28714416d7d2992c8d969fde883fd52835a1
Ariell-jsj <king4r13l@gmail.com>
2026-04-24 20:10:01 -0500

    Se crea funcion graficarRectangulo y se reutiliza en gato y comida

--- cazando.js
+++ cazando.js
@@ -28,14 +28,18 @@ function iniciarJuego() {
   graficarComida();
 }
 
+// FUNCIÓN GENERAL
+function graficarRectangulo(x, y, ancho, alto, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, ancho, alto);
+}
+
 // Dibujar gato
 function graficarGato() {
-  ctx.fillStyle = "orange";
-  ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+  graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "black");
 }
 
 // Dibujar comida
 function graficarComida() {
-  ctx.fillStyle = "green";
-  ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
+  graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "yellow");
 }
