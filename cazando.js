d25b25c1169754566fad9a7c06ceead1b62cce08
Ariell-jsj <king4r13l@gmail.com>
2026-05-06 20:58:04 -0500

    Aleatorio en X y Y

--- cazando.js
+++ cazando.js
@@ -1,96 +1,130 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
-// VARIABLES
-let gatoX = 0;
-let gatoY = 0;
-let comidaX = 0;
-let comidaY = 0;
+// VARIABLES DEL JUEGO
+let gatoX = 220;
+let gatoY = 230;
+
+let comidaX = 460;
+let comidaY = 460;
+
+let puntos = 0;
+let tiempo = 10;
+let velocidad; 
 
 // CONSTANTES
+
 const ANCHO_GATO = 60;
 const ALTO_GATO = 40;
 
 const ANCHO_COMIDA = 40;
 const ALTO_COMIDA = 40;
 
+
 // INICIAR JUEGO
-function iniciarJuego() {
-  gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
-  gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
 
-  comidaX = canvas.width - ANCHO_COMIDA;
-  comidaY = canvas.height - ALTO_COMIDA;
+function iniciarJuego(){
 
-  graficarGato();
-  graficarComida();
+    clearInterval(velocidad);
+
+    velocidad = setInterval(contarTiempo,1000);
+
+    actualizarPantalla();
 }
-
-// LIMPIAR CANVAS
-function limpiarCanva() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-// DETECTAR COLISIÓN
-function detectarColision() {
-  if (
-    gatoX < comidaX + ANCHO_COMIDA &&
-    gatoX + ANCHO_GATO > comidaX &&
-    gatoY < comidaY + ALTO_COMIDA &&
-    gatoY + ALTO_GATO > comidaY
-  ) {
-    alert("¡El gato atrapó la comida!");
-  }
-}
-
-// MOVER IZQUIERDA
-function moverIzquierda() {
-  gatoX = gatoX - 10;
-  limpiarCanva();
-  graficarGato();
-  graficarComida();
-  detectarColision();
-}
-
-// MOVER DERECHA
-function moverDerecha() {
-  gatoX = gatoX + 10;
-  limpiarCanva();
-  graficarGato();
-  graficarComida();
-  detectarColision();
-}
-
-// MOVER ARRIBA
-function moverArriba() {
-  gatoY = gatoY - 10;
-  limpiarCanva();
-  graficarGato();
-  graficarComida();
-  detectarColision();
-}
-
-// MOVER ABAJO
-function moverAbajo() {
-  gatoY = gatoY + 10;
-  limpiarCanva();
-  graficarGato();
-  graficarComida();
-  detectarColision();
-}
-
-// FUNCIÓN GENERAL
+//Dibujos de Gato y Comida
 function graficarRectangulo(x, y, ancho, alto, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, ancho, alto);
 }
 
-// DIBUJAR GATO
+function graficarComida() {
+  graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "yellow");
+}
+
 function graficarGato() {
   graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "black");
 }
 
-// DIBUJAR COMIDA
-function graficarComida() {
-  graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "yellow");
+//Limpiar el canvas
+function limpiarCanvas(){
+   ctx.fillStyle="#87CEEB";
+   ctx.fillRect(0,0,500,300);
+
+ctx.fillStyle="#87CEEB";
+
+    ctx.fillRect(0,0,500,500);
 }
+
+function actualizarPantalla(){
+  limpiarCanvas();
+  graficarComida();
+  graficarGato();
+
+}
+
+// Movimiento del gato
+function moverIzquierda(){
+  gatoX=gatoX-10;
+  actualizarPantalla();
+  detectarColision();
+}
+
+function moverDerecha(){
+  gatoX=gatoX+10;
+  actualizarPantalla();
+  detectarColision();
+}
+
+function moverArriba(){
+  gatoY=gatoY-10;
+  actualizarPantalla();
+  detectarColision();
+}
+
+function moverAbajo(){
+  gatoY=gatoY+10;
+  actualizarPantalla();
+  detectarColision();
+}
+
+// DETECTAR COLISION
+function detectarColision(){
+  if(
+    gatoX + ANCHO_GATO >= comidaX &&
+    gatoX <= comidaX + ANCHO_COMIDA &&
+    gatoY + ALTO_GATO >= comidaY &&
+    gatoY <= comidaY + ALTO_COMIDA
+  ){
+
+    alert("¡El gato atrapó la comida!");
+    aparecerComida();
+    puntos=puntos+1;
+    mostrarEnSpan("puntos",puntos);
+    }
+}
+
+function aparecerComida(){
+  comidaX=generarAleatorio(0, canvas.width - ANCHO_COMIDA);
+  comidaY=generarAleatorio(0, canvas.height - ALTO_COMIDA);
+  actualizarPantalla();
+}
+
+function mostaraMensaje(texto, tiempo){
+    let mensaje = document.getElementById("mensaje");
+    mensaje.textContent = texto;
+
+    setTimeout(function(){
+        mensaje.textContent = "";
+    }, tiempo);
+}
+
+function contarTiempo(){
+    tiempo--;
+    mostrarEnSpan("tiempo", tiempo);
+
+    if(tiempo == 0){
+        alert("Se acabó el tiempo");
+        clearInterval(velocidad);
+    }
+}
+
